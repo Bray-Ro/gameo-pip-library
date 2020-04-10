3c29ada7373de0092905d37d2b6c34188f36ad74
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-04-10 18:43:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'gameo',         # How you named your package folder (MyLib)
+  packages = ['gameo'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  description = 'the library for the gameo engine',   # Give a short description about your library
+  author = 'Rohan samra-oneill',                   # Type in your name
+  author_email = 'rohanisdeadly@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/rohanThegreaTT/gameo-pip-library',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['game-engine', 'gameo],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
           'beautifulsoup4',
+          'OpenGL.GL',
+          'OpenGL.GLUT',
+          'OpenGL.GLU'
+    
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
